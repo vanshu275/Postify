cb7dca81f2592db2e69429322a15378c2095874e
Vansh Upadhyay <upadhyayvansh92@gmail.com>
2026-04-08 16:07:10 +0530

    changes in sidenav

--- Client/src/components/App-sidebar2.tsx
+++ Client/src/components/App-sidebar2.tsx
@@ -1,5 +1,12 @@
-import { Link } from "react-router-dom"
-import { Home, MessageCircle, PlusSquare, RectangleGoggles  } from "lucide-react"
+import { Link, useLocation } from "react-router-dom"
+import { useState } from "react"
+import {
+  Home,
+  MessageCircle,
+  PlusSquare,
+  RectangleGoggles,
+  UserRound,
+} from "lucide-react"
 
 const navItems = [
   { icon: Home, label: "Home", href: "/" },
@@ -7,22 +14,67 @@ const navItems = [
   { icon: PlusSquare, label: "Create", href: "/create" },
 ]
 
-
 export const AppSidebar2 = () => {
+  const [hovered, setHovered] = useState(false)
+  const location = useLocation()
+
   return (
-    <div className="h-screen w-[14vw] border-r pl-4">
-      <div className="text-2xl ">
-            <RectangleGoggles />
+    <div className="flex h-screen w-[14vw] flex-col justify-between px-3 py-4 transition-all duration-800 ease-in-out">
+      {/* Top Section */}
+      <div>
+        <div className="mb-8 ml-1 flex justify-start text-2xl">
+          <RectangleGoggles size={35} />
+        </div>
+
+        <div
+          className="flex flex-col gap-6"
+          onMouseLeave={() => setHovered(false)}
+        >
+          {navItems.map((item) => {
+            return (
+              <Link
+                to={item.href}
+                key={item.label}
+                className={`} flex items-center gap-4 rounded-lg p-2 transition ${hovered ? "hover:bg-[#25282C]" : ""} hover:text-black`}
+              >
+                <item.icon
+                  onMouseEnter={() => setHovered(true)}
+                  size={24}
+                  className={`text-white`}
+                  strokeWidth={2}
+                />
+
+                {hovered && (
+                  <span className={`text-[15px] text-white`}>{item.label}</span>
+                )}
+              </Link>
+            )
+          })}
+        </div>
       </div>
 
-      {navItems.map((items) => (
-        <div className="flex" key={items.label}>
-          <Link to={items.href} className="flex">
-            <items.icon />
-            <h2>{items.label}</h2>
-          </Link>
-        </div>
-      ))}
+      {/* Bottom Profile */}
+      <div>
+        {(() => {
+          return (
+            <Link
+              to="/profile"
+              className={`flex items-center gap-4 rounded-lg p-2 transition ${hovered ? "hover:bg-[#25282C]" : ""}`}
+            >
+              <UserRound
+                onMouseEnter={() => setHovered(true)}
+                onMouseLeave={() => setHovered(false)}
+                size={hovered ? 22 : 24}
+                className={`text-white`}
+              />
+
+              {hovered && (
+                <span className={`text-[15px] text-white`}>Profile</span>
+              )}
+            </Link>
+          )
+        })()}
+      </div>
     </div>
   )
 }
